b285571e60e5618a7029db86b2641576254a8269
Collectathon <60051762+Collectathon@users.noreply.github.com>
2020-07-07 11:52:24 +1000

    Create PCPartPicker-AU.user.js

---
+++ PCPartPicker-AU.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        PCPartPicker AU
+// @match       *://*.pcpartpicker.com/*
+// @grant       none
+// @version     1.0
+// @author      Baconn
+// @description 7/7/2020, 11:44:23 AM
+// ==/UserScript==
+
+if (location.hostname.startsWith('pcpartpicker.com')) {
+
+  window.stop();
+  location.hostname = 'au.pcpartpicker.com';
+} 
